43ba012e016a4658d4b6cae32e78a2f25a171901
Marianela Carrasco <marianela.carrascoi36@gmail.com>
2022-06-06 14:07:03 -0300

    tp funciones

---
+++ JS/archivo.js
@@ -0,0 +1,14 @@
+
+function nombreArtista(){
+
+    let nombreIngresado=prompt("Ingresa el nombre de tu artista")
+    if(nombreIngresado=="Los Tipitos"){
+        alert ("Ya tienes tu entrada")
+    }else if (nombreIngresado=="Mana"){
+        alert ("Funcion cancelada")
+    }else{
+        alert ("Entradas agotadas")
+    }    
+}
+nombreArtista();
+
